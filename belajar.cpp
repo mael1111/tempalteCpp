97c5a6dfef12f8299254cac7eb8205e6d421cb82
maelhsnh0110@gmail.com <ilmahasanah1111@gmail.com>
2025-04-08 09:40:21 +0800

    Test environment C++ Developer

--- belajar.cpp
+++ belajar.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
-int main(){
-      printf("Hello world");
-      return 0;
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Coding Is Fun !" << endl;
+    return 0;
 }
